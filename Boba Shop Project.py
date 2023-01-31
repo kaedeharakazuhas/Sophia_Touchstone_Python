06b78fe13fe9f8f5a53d63fa8745377e7afd3a13
Lina <dategumis@gmail.com>
2023-01-30 19:14:37 -0800

    Update 8

--- Boba Shop Project.py
+++ Boba Shop Project.py
@@ -5,9 +5,15 @@ def greeting():
     print(" |    ☆*.｡.o(≧▽≦)o.｡.*☆      |")
     print(" *~~~~~~~~~~~~~~~~~~~~~~~~~~~* ")
     user_name = input("What is the name for the order? ")
-    print("Thank you, ", user_name, ". What can I get for you today?", sep='')
-    print("")
-    boba_ordering()
+    print("Thank you, ", user_name, ".", sep='')
+    get_money()
+
+
+def get_money():
+    customer_money = float(input("How much do you have to spend at Pyboba "
+                                 "today? Please enter your money without the "
+                                 "dollar sign and with decimals: "))
+    boba_ordering(customer_money)
 
 
 def display_menu():
@@ -25,14 +31,15 @@ def display_menu():
     print("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*")
 
 
-def boba_ordering():
+def boba_ordering(customer_money):
     customer_order = []
     prices = []
     customer_subtotal = 0.00
     still_a_customer = True
     while still_a_customer:
         display_menu()
-        my_first_drink = input("Please select a drink from the menu using a number or press 0 to cancel/quit: ")
+        my_first_drink = input("Please select a drink from the menu using a "
+                               "number or press 0 to cancel/quit: ")
         try:
             customer_answer = int(my_first_drink)
         except ValueError:
@@ -44,7 +51,8 @@ def boba_ordering():
             customer_order.append("Classic Milk Tea (16oz)")
             price = float(input("Please enter the price of the item: "))
             prices.append(price)
-            continue_order = input("Would you like to order something else? Please Enter 'Yes' or 'No'. ")
+            continue_order = input("Would you like to order something else? "
+                                   "Please Enter 'Yes' or 'No'. ")
             if continue_order.capitalize() == "Yes":
                 still_a_customer = True
             elif continue_order.capitalize() == "No":
@@ -58,7 +66,8 @@ def boba_ordering():
             customer_order.append("Classic Milk Tea (24oz)")
             price = float(input("Please enter the price of the item: "))
             prices.append(price)
-            continue_order = input("Would you like to order something else? Please Enter 'Yes' or 'No'. ")
+            continue_order = input("Would you like to order something else? "
+                                   "Please Enter 'Yes' or 'No'. ")
             if continue_order.capitalize() == "Yes":
                 still_a_customer = True
             elif continue_order.capitalize() == "No":
@@ -72,7 +81,8 @@ def boba_ordering():
             customer_order.append("Passion Fruit Green Tea (16oz)")
             price = float(input("Please enter the price of the item: "))
             prices.append(price)
-            continue_order = input("Would you like to order something else? Please Enter 'Yes' or 'No'. ")
+            continue_order = input("Would you like to order something else? "
+                                   "Please Enter 'Yes' or 'No'. ")
             if continue_order.capitalize() == "Yes":
                 still_a_customer = True
             elif continue_order.capitalize() == "No":
@@ -86,7 +96,8 @@ def boba_ordering():
             customer_order.append("Passion Fruit Green Tea (24oz)")
             price = float(input("Please enter the price of the item: "))
             prices.append(price)
-            continue_order = input("Would you like to order something else? Please Enter 'Yes' or 'No'. ")
+            continue_order = input("Would you like to order something else? "
+                                   "Please Enter 'Yes' or 'No'. ")
             if continue_order.capitalize() == "Yes":
                 still_a_customer = True
             elif continue_order.capitalize() == "No":
@@ -100,7 +111,8 @@ def boba_ordering():
             customer_order.append("Mango Smoothie (24oz)")
             price = float(input("Please enter the price of the item: "))
             prices.append(price)
-            continue_order = input("Would you like to order something else? Please Enter 'Yes' or 'No'. ")
+            continue_order = input("Would you like to order something else? "
+                                   "Please Enter 'Yes' or 'No'. ")
             if continue_order.capitalize() == "Yes":
                 still_a_customer = True
             elif continue_order.capitalize() == "No":
@@ -127,7 +139,17 @@ def boba_ordering():
 
     print(f"The total comes out to: ${customer_total:.2f} after tax.")
     print("")
-    farewell()
+
+    if customer_money >= customer_total:
+        customer_change = customer_money - customer_total
+        print("Great! You have enough money for this order.")
+        print(f"Your change is: ${customer_change:.2f}.")
+        print("")
+        farewell()
+    else:
+        print("Sorry, but you don't have enough money for this order. Please "
+              "restart this program again.")
+        print("")
 
 
 def farewell():
